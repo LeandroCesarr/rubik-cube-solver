bebc52b4a1ba309564a5b0a272db4cece8238855
Leandro C. Silva <leandrocesar49@hotmail.com>
2022-11-03 17:08:17 -0300

    test: improve layer solver tests

--- tests/LayerSolver.test.ts
+++ tests/LayerSolver.test.ts
@@ -1,28 +1,74 @@
-import { Cube } from "@/lib/Cube";
-import { LayerSolver } from "@/lib/LayerSolver";
-import { describe, expect, it } from "vitest";
+import { Cube } from '@/lib/Cube';
+import { LayerSolver } from '@/lib/LayerSolver';
+import { describe, expect, it } from 'vitest';
+import { defaultState } from './data/cube/defaultState';
 
 const state = [
-  [36, 11, 27, 42, 5, 13, 30, 35, 37],
-  [39, 20, 43, 6, 14, 40, 12, 49, 10],
-  [28, 22, 54, 2, 23, 26, 46, 47, 45],
-  [34, 38, 7, 24, 32, 31, 52, 8, 1],
-  [18, 44, 48, 15, 41, 29, 3, 4, 16],
-  [19, 33, 9, 17, 50, 53, 25, 51, 21],
+  [12, 42, 45, 24, 5, 4, 52, 40, 43],
+  [48, 17, 30, 33, 14, 47, 46, 11, 36],
+  [34, 31, 37, 49, 23, 6, 21, 20, 39],
+  [27, 13, 25, 35, 32, 29, 10, 26, 16],
+  [1, 2, 28, 44, 41, 51, 7, 8, 19],
+  [54, 53, 3, 22, 50, 38, 18, 15, 9],
 ];
 
 const makeSUT = () => {
-  const cube = new Cube(state);
+  const cube = new Cube();
+
+  cube.shuffle();
+
   const solver = new LayerSolver(cube);
 
   return {
     cube,
-    solver
-  }
-}
+    solver,
+  };
+};
 
-describe("LayerSolver", () => {
-  it("Should solve cube", () => {
+describe('LayerSolver', () => {
+  it('Should have made the white cross', async () => {
+    // Arrange
+    const { solver, cube } = makeSUT();
+
+    // Act
+    await solver.solve();
+
+    // Assert
+    expect(cube.faces[5][1]).toBe(defaultState[5][1]);
+    expect(cube.faces[5][3]).toBe(defaultState[5][3]);
+    expect(cube.faces[5][5]).toBe(defaultState[5][5]);
+    expect(cube.faces[5][7]).toBe(defaultState[5][7]);
+  });
+
+  it('Should have solved first layer', async () => {
+    // Arrange
+    const { solver, cube } = makeSUT();
+
+    // Act
+    await solver.solve();
+
+    // Assert
+    expect(solver.isBottomLayerSolved).toBeTruthy();
+  });
+
+  it('Should returns how to solve cube', async () => {
+    // Arrange
+    const { solver, cube } = makeSUT();
+
+    // Act
+    expect(cube.faces).toStrictEqual(state);
+
+    const solve = await solver.getSolve();
+
+    cube.moveMany(solve);
+
+    console.log({ solve });
+
+    // Assert
+    expect(cube.isSolved).toBeTruthy();
+  });
+
+  it('Should solved cube', () => {
     // Arrange
     const { solver, cube } = makeSUT();
 
@@ -31,5 +77,5 @@ describe("LayerSolver", () => {
 
     // Assert
     expect(cube.isSolved).toBeTruthy();
-  })
-})
+  });
+});
